11eb9dc4a64484d0f2fbc71c760632db1eaa581c
Egor Bazaleev <egor.bazaleev@inexto.ru>
2026-07-09 20:11:41 +0300

    добавлены 3 компонента кнопка, заголовок, параграф

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Button from './components/Button/Button';
+import Header from './components/Header/Header';
+import Paragraph from './components/Paragraph/Paragraph';
+
+function App() {
+
+  return (
+    <>
+      <Button/>
+      <Header/>
+      <Paragraph/>
+    </>
+  );
+}
+
+export default App;
